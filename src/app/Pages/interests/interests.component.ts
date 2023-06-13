81de8d5d7445972146bda78f078d8dd4a66da35f
Mohamed Ben Othman <Mohamedbenothman2@outlook.com>
2023-06-13 18:40:15 +0100

    Finished the interests screen

--- src/app/Pages/interests/interests.component.ts
+++ src/app/Pages/interests/interests.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-interests',
   templateUrl: './interests.component.html',
-  styleUrls: ['./interests.component.css']
+  styleUrls: ['./interests.component.css'],
+  animations: [
+    trigger('imgAnimation', [
+      state('initial', style({
+        top: '34.3%',
+        right: '33.1%'
+      })),
+      state('final', style({
+        top: '49.5%',
+        right: '20.7%'
+      })),
+      transition('initial => final', animate('500ms')),
+      transition('final => initial', animate('500ms'))
+    ])
+  ]
 })
 export class InterestsComponent implements OnInit {
+
+  animationState: string = 'initial';
+  displayModal: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  toggleAnimationState() {
+    this.animationState = this.animationState === 'initial' ? 'final' : 'initial';
+    setTimeout(() => {
+      this.toggleModalDisplay();
+    }, 1200);
+  }
+
+  toggleModalDisplay = () => this.displayModal = !this.displayModal;
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,7 +37,8 @@ import { GuitarComponent } from './Pages/guitar/guitar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
